aa5f8d86b831b885f83f36f1952169dabbcc48ff
drowe <daniel.rowe9193@gmail.com>
2026-04-08 16:56:04 +0200

    Implemented dataset property to Topography.

--- src/palm_cbean/topography.py
+++ src/palm_cbean/topography.py
@@ -1,12 +1,10 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import rasterio
 import xarray
-from pyproj import Transformer
 from scipy.ndimage import zoom
 
 
-def make_even(data: np.ndarray):
+def make_even(data: np.ndarray) -> np.ndarray:
     """Checks if any dimension of the array is odd and pads it so that it becomes even. PALM demands that the topography files have even dimensions."""
     if data.shape[0] % 2 != 0:
         data = np.pad(data, ((0, 1), (0, 0)), mode='constant')
@@ -30,36 +28,41 @@ class Topography:
 
         self.x, self.y = np.meshgrid(np.arange(self.source.height), np.arange(self.source.width), indexing="ij")
 
-        self.norm_x: np.ndarray | None = None
-        self.norm_y: np.ndarray | None = None
-
-        # self.x, self.y = rasterio.transform.xy(self.source.transform, self.rows, self.columns)
-        #
-        # self.transformer = Transformer.from_crs(self.source.crs, "EPSG:4326", always_xy=True)
-        # self._lon, self._lat = self.transformer.transform(self.x, self.y)
-        #
-        # self.latitude = np.array(self._lat).reshape(self.source.height, self.source.width)
-        # self.longitude = np.array(self._lon).reshape(self.source.height, self.source.width)
-
-        self.dataset: xarray.Dataset | None = None
-
     @property
-    def shape(self):
+    def shape(self) -> tuple[int]:
         """The shape of the topography file."""
         return self.elevation.shape
 
     @property
+    def dataset(self):
+        """Construct and prepare a xarray dataset for the elevation data."""
+
+        x = self.x[:, 0]
+        y = self.y[0, :]
+
+        norm_x = (x - x.min()) / (x.max() - x.min())
+        norm_y = (y - y.min()) / (y.max() - y.min())
+
+        dataset = xarray.Dataset(
+            data_vars={
+                "elevation": (("x", "y"), self.elevation)
+            },
+            coords={
+                "norm_x": ("x", norm_x),
+                "norm_y": ("y", norm_y)
+            }
+        )
+
+        return dataset
+
     def mask(self):
         """Mask missing values in the topography file"""
 
-        # Apply linear interpolation using the nearest members around the missing value grid point.
-
-        self.elevation[self.elevation == -3.402823466385288598e+38] = 0
-        self.elevation[self.elevation == -3.402820018375655977e+38] = 0
+        self.elevation[self.elevation == -3.402823466385288598e+38] = np.nan
+        self.elevation[self.elevation == -3.402820018375655977e+38] = np.nan
 
         return self
 
-    @property
     def flip(self):
         """Flips the elevation array to match cardinal north."""
 
@@ -67,21 +70,9 @@ class Topography:
 
         return self
 
-    @property
     def make_shape_even(self):
         """Pads the elevation if the shape of any dimension is odd. This should be called after downscaling or padding."""
         self.elevation = make_even(self.elevation)
-
-        return self
-
-    @property
-    def normalise_axes(self):
-        """Normalise the x and y axes to fall between 0 and 1."""
-        x = self.x[:, 0]
-        y = self.y[0, :]
-
-        self.norm_x = (x - x.min()) / (x.max() - x.min())
-        self.norm_y = (y - y.min()) / (y.max() - y.min())
 
         return self
 
@@ -107,17 +98,4 @@ class Topography:
         np.savetxt(f"{output_directory}\\{output_filename}_{1 / self.resolution}m_topo", self.elevation, fmt="%.1d")
         return self
 
-    def to_xarray(self):
-        """Construct a xarray dataset for the elevation data."""
 
-        self.dataset = xarray.Dataset(
-            data_vars={
-                "elevation": (("x", "y"), self.elevation)
-            },
-            coords={
-                "norm_x": ("x", self.norm_x),
-                "norm_y": ("y", self.norm_y)
-            }
-        )
-
-        return self
