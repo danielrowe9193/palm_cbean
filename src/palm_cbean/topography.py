d6abfcf27f348e228e0d04e64b8968469a983d2d
drowe <daniel.rowe9193@gmail.com>
2026-06-12 20:36:24 +0200

    Added ToyTopography class for learning purposes.

--- src/palm_cbean/topography.py
+++ src/palm_cbean/topography.py
@@ -127,6 +127,8 @@ class Topography:
         None
         """
 
+        self.elevation[self.elevation == np.nan] = 0
+
         self.elevation = gaussian_filter(self.elevation, sigma=Constants.SIGMA)
 
         return None
@@ -149,3 +151,75 @@ class Topography:
         print(f"\nFile saved to {topography_file_path}\n")
 
         return None
+
+
+class ToyTopography:
+    """
+    Class containing utilities for creating toy buildings to be used with PALM.
+
+    Useful for testing and learning the PALM model system. Possible future applications testing fluid flow through buildings.
+    """
+
+    def __init__(self, size_of_domain: tuple[int]) -> None:
+        """
+        Creates the domain on which the toy buildings should be placed.
+
+        Uses size_of_domain to create an array of zeros, which represents the surface. This is the canvas on which
+        further buildings can be placed.
+
+        :param size_of_domain: The size of the domain to be simulated, expressed as a tuple of integers. Also represents the
+        shape of the domain which is critical for the namelist file and running PALM.
+        """
+
+        self.size_of_domain = size_of_domain
+
+        self.topography = np.zeros(self.shape)
+
+    def add_building(self, x_span: tuple[int], y_span: tuple[int], height: int) -> None:
+        """
+        Adds a building to the topography.
+
+        This function simply defines the grid points in x, y, and z that a building will occupy. The actually dimensions of the
+        building are established in the PALM simulation when you set `dx` and `dy`.
+
+
+
+        :param x_span: The span of the building in the x-direction. It is defined by giving the leftmost x position and rightmost x position of the building.
+        An x_span=(2, 10), means that the building will occupy a region ranging from x=2 to x=10.
+        :param y_span: The span of the building in the y-direction. It is defined by giving the bottommost y position and uppermost y position of the building.
+        A y_span=(2, 10), means that the building will occupy a region ranging from y=2 to y=10.
+        :param height: The height of the building.
+        :return:
+        """
+
+        self.topography[x_span[0]:x_span[1], y_span[0], y_span[1]] = height
+
+        return None
+
+    def save_topofile(self, destination_directory: str, filename: str) -> None:
+        """
+        Saves the toy topography file to the directory of the user's choice.
+
+        The filename should not have any extensions, as it will be saved as an ASCII file. Deviating from this
+        may cause errors.
+
+        This should be called after all the desired buildings are added. If it is called before, then the topography
+        file will not contain all the buildings the user created.
+
+        :param destination_directory: The directory to store the topography file. For the usages of PALM, this should be the
+        INPUT/ directory of the experiment.
+        :param filename: The name of the topography file. It should have the same name as the experiment directory and have
+        _topo at the end. For example, if the experiment is stored in toy_model/, then the topography file should be called
+        toy_model_topo.
+        :return: None
+        """
+
+        filepath = Path(destination_directory) / filename
+
+        np.savetxt(filepath, self.topography, fmt="%.0f")
+
+        print(f"The ToyTopography file was saved to {filepath}")
+
+        return None
+
+
