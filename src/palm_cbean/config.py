7c5602a6cef88adfa4da51cd37968f2cca55801d
drowe <daniel.rowe9193@gmail.com>
2026-06-15 20:46:01 +0200

    Implemented first variables to PlotElements class.

--- src/palm_cbean/config.py
+++ src/palm_cbean/config.py
@@ -1,12 +1,31 @@
 from pathlib import Path
 
-class Constants:
 
+class Constants:
     plot_storage_directory = Path("../../plots/")
+
+    _xy_data = Path("../../data/flow_around_cube_cyclic_xy.000.nc")
+    _xz_data = Path("../../data/bds_test9_xz.000.nc")
 
     SIGMA = 1.5
 
 
 class PlotElements:
+    """
+    Container for all plot decorators.
+    """
 
-    ...
+    plot_elements = {
+        "w_speed": dict(
+            long_name="Wind Speed",
+            units=r"$ms^{-1}$"
+        ),
+        "u": dict(
+            long_name="Zonal Component of Wind",
+            units=r"$ms^{-1}$"
+        ),
+        "v": dict(
+            long_name="Meridional Component of Wind",
+            units=r"$ms^{-1}$"
+        ),
+    }
