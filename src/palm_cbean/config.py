14d13025f9a568fc4cf4cb147e4d5213bc3f1361
drowe <daniel.rowe9193@gmail.com>
2026-06-16 10:09:54 +0200

    Updated PlotElements with information for terrain plotting.

--- src/palm_cbean/config.py
+++ src/palm_cbean/config.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from pathlib import Path
 
 
@@ -16,6 +18,15 @@ class PlotElements:
     """
 
     plot_elements = {
+        "elevation": dict(
+            long_name="Elevation",
+            units=r"$m$",
+            xlabel=dict(x="x", norm_x="normalised x"),
+            ylabel=dict(y="y", norm_y="normalised y"),
+            cmap="terrain",
+            contour_line_levels=[0, 1, 5, 10, 25, 50, 100, 200, 300, 400],
+            contour_fill_levels=np.linspace(0, 400, 21)
+        ),
         "w_speed": dict(
             long_name="Wind Speed",
             units=r"$ms^{-1}$"
