2cf1d9a84b481ba188892b4f2db12bf4760464d7
drowe <daniel.rowe9193@gmail.com>
2026-06-19 13:18:11 +0200

    Implemented PlotElements attributes for different variables.

--- src/palm_cbean/config.py
+++ src/palm_cbean/config.py
@@ -6,6 +6,8 @@ from pathlib import Path
 class Constants:
     plot_storage_directory = Path("../../plots/")
 
+    bds_topography_path = Path("../../data/BRB_DEM_10M_UTM21N.tif")
+
     _xy_data = Path("../../data/flow_around_cube_cyclic_xy.000.nc")
     _xz_data = Path("../../data/bds_test9_xz.000.nc")
 
@@ -25,11 +27,16 @@ class PlotElements:
             ylabel=dict(y="y", norm_y="normalised y"),
             cmap="terrain",
             contour_line_levels=[0, 1, 5, 10, 25, 50, 100, 200, 300, 400],
+            colors="black",
+            linewidths=0.5,
             contour_fill_levels=np.linspace(0, 400, 21)
         ),
         "w_speed": dict(
             long_name="Wind Speed",
-            units=r"$ms^{-1}$"
+            units=r"$ms^{-1}$",
+            cmap="turbo",
+            levels=np.linspace(0, 100, 101),
+            cb_ticks=np.linspace(0, 100, 11),
         ),
         "u": dict(
             long_name="Zonal Component of Wind",
