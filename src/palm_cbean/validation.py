fae5f09f0c5b20e8cc4628f37e8a0399a5562790
drowe <daniel.rowe9193@gmail.com>
2026-07-03 21:08:22 +0200

    Add validation for palmout.py object.

--- src/palm_cbean/validation.py
+++ src/palm_cbean/validation.py
@@ -4,7 +4,7 @@ import palmout
 def require_palmout_xy(obj) -> None:
     if not isinstance(obj, palmout.PalmOutXY):
         raise TypeError(
-            "Expected type of PalmOut is PalmOutXY."
+            f"Expected PalmOutXY, got {type(obj).__name__}"
         )
 
     return None
@@ -13,7 +13,7 @@ def require_palmout_xy(obj) -> None:
 def require_palmout_xz(obj) -> None:
     if not isinstance(obj, palmout.PalmOutXZ):
         raise TypeError(
-            "Expected type of PalmOut is PalmOutXZ."
+            f"Expected PalmOutXZ, got {type(obj).__name__}"
         )
 
     return None
