8f2201c1e19b133ffbc8ae182a5a996eca9cd839
drowe <daniel.rowe9193@gmail.com>
2026-07-07 00:05:54 +0200

    Update main.py to reflect package changes.

--- src/palm_cbean/main.py
+++ src/palm_cbean/main.py
@@ -15,6 +15,15 @@ utils.DirectoryManagement.make_plots_directory()
 bds_topo = Topography(
     filepath=config.Constants.bds_topography_path
 )
+bds_topo.mask()
+bds_topo.flip()
+bds_topo.make_shape_even()
+bds_topo.pad(amount=5000)
+bds_topo.downscale(final_resolution=100)
+bds_topo.mask()
+print(bds_topo.shape)
+
+plot.PlotTopography(bds_topo).plot_elevation()
 
 palmout_xy = palmout.PalmOutXY(
     palm_out_filepath=Path("../../data/toy_model_xy.002.nc")
@@ -28,10 +37,10 @@ palmout_xz = palmout.PalmOutXZ(
 palmout_xz.normalise()
 palmout_xz.update_data_with_normalised_coords()
 
-print(palmout_xz.data)
+# print(palmout_xz.data)
 
-anim = animate.Animator(palm_out=palmout_xz)
+# anim = animate.Animator(palm_out=palmout_xz)
 # anim.generate_frames_xy(variable="wspeed", zu_xy_index=2)
-anim.generate_frames_xz(variable="w_xz", y_xz_index=2)
+# anim.generate_frames_xz(variable="w_xz", y_xz_index=3)
 # anim.animate_mp4(mp4_name="test.mp4", new_frame_directory_name="toy_model_xy.002")
 
