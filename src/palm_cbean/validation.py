07240ec576ec87e8a73df7c9db198fe31a63a2f7
drowe <daniel.rowe9193@gmail.com>
2026-07-09 14:54:50 +0200

    Add type checking for PalmOut objects.

--- src/palm_cbean/validation.py
+++ src/palm_cbean/validation.py
@@ -1,6 +1,18 @@
 import palmout
 
 
+def is_palmout_xy(obj) -> bool:
+    """Checks if PalmOutXY."""
+    if isinstance(obj, palmout.PalmOutXY):
+        return True
+
+
+def is_palmout_xz(obj) -> bool:
+    """Checks if PalmOutXY."""
+    if isinstance(obj, palmout.PalmOutXZ):
+        return True
+
+
 def require_palmout_xy(obj) -> None:
     if not isinstance(obj, palmout.PalmOutXY):
         raise TypeError(
