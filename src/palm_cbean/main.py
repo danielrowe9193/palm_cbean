ae1075389cec3cdfa42ef93e924b9a765c5f2b08
drowe <daniel.rowe9193@gmail.com>
2026-07-13 22:02:47 +0200

    Temporary change to main.py for testing.

--- src/palm_cbean/main.py
+++ src/palm_cbean/main.py
@@ -28,11 +28,13 @@ po = None
 
 if args.po == "xy":
     po = palmout.Loaders.load_xy(args.dp)
+    print(type(po))
 if args.po == "xz":
     po = palmout.Loaders.load_xz(args.dp)
+    print(type(po))
 
 anim = animate.Animator(palm_out=po)
 # anim.generate_frames(variable=args.var, index=args.i)
-anim.generate_frames_xy(variable=args.var, zu_xy_index=args.i)
+# anim.generate_frames_xy(variable=args.var, zu_xy_index=args.i)
 # anim.generate_frames_xz(variable="w_xz", y_xz_index=3)
 anim.animate_mp4(mp4_name=args.an, new_frame_directory_name=args.fdn)
